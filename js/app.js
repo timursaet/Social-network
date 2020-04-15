949f86950af62bfaf7647810ede0d0c82d9e1651
timursaet <s.timursaet@yandex.ru>
2020-04-15 18:09:38 +0500

    add remove event

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
 "use strict";
 const details = document.getElementsByClassName('content__data__info__details_cheked')[0];
 const viewDetails = document.getElementsByClassName('content__data__info__details')[0];
+const avatarPerson = document.getElementsByClassName('side-Bar__avatar')[0];
+const editAvatarPerson = document.getElementsByClassName('side-Bar__avatar_edit')[0];
+
 
 details.addEventListener('click', () => {
-    viewDetails.style.display = 'none';
-    details.innerHTML = 'Показать подробности';
+        viewDetails.style.display = 'none';
+        details.innerHTML = 'Показать подробности';
+        document.removeEventListener('click');
 });
+
+avatarPerson.onmouseover = function() {
+    editAvatarPerson.style.display = 'block';
+}
+
+avatarPerson.onmouseout = function() {
+       editAvatarPerson.style.display = 'none';
+}
